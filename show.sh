74489f850e6ff93650841ae4100814eea73a9004
t <ssstrz@gmail.com>
2017-01-10 17:48:21 +0100

    fixed bug with children

--- show.sh
+++ show.sh
@@ -5,16 +5,25 @@ if [ -z "$1" ]; then
 fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 user=$(cat "$DIR/user.txt")
-
-readarray -t lst < <( hg log --user "$user" -r "head() and not closed()" --template "{branch}\n" | python -c "import sys; print ' '.join(set([l for l in sys.stdin.read().splitlines() if '"$branch"' != l and l.startswith('"$branch"')]))")
-readarray -t lst2 < <( hg log --user "$user" -r "children(ancestor($branch)) and merge()" --template "{branch}\n" | python -c "import sys; print ' '.join(set([l for l in sys.stdin.read().splitlines()]))")
-
-#lst+=lst2
-for I in $lst2
+readarray -t lst < <( hg log --user "$user" -r "children(branch($branch))" --template "{branch}\n" | python -c "import sys; print ' '.join(set([l for l in sys.stdin.read().splitlines()]))")
+my_lst=()
+for i in $lst
 do
-    lst=$lst" $I"
+	author=$(hg log -r "first(branch('$i'))" --template "{author}")
+	if [ "$user" == "$author" ];
+		then
+			my_lst+=' '$i
+	fi
 done
-
+lst=()
+for i in $my_lst
+do
+	opened=$(hg log -r "not closed() and (last(branch($i)))" --template "{branch}")
+	if [ ! -z "$opened" ]; then
+		lst+=' '$i
+		#echo $i, open
+	fi
+done
 echo 'Author (in sprint: '$branch')'
 echo $user
 echo ''
