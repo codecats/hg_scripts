7da8bc0db2633cdefa7730e2614690622f0b607e
ssstrz <codecats@gmail.com>
2017-01-13 22:53:42 +0100

    info show last merged

--- info.sh
+++ info.sh
@@ -8,14 +8,14 @@ hg log -r "branch($branch) and not merge()" --template "\x1B[32m{node|formatnode
 closed=$(hg log -r "branch($branch) and closed() and head()" --template "{date|age}")
 last_merge=$(hg log -r "last(children(branch($branch)) and merge())" --template "{branch} {date|age} \x1B[32m{node|formatnode}\x1B[0m")
 
-
+echo ""
 if [ ! -z "$closed" ] && [ ! -z "$last_merge" ]; then
-	echo ""
 	echo -e '⋋ \e[33mMerged\x1B[0m to '$last_merge
+elif [ ! -z "$last_merge" ]; then
+	echo -e "⋋ \e[33mLast Merged\x1B[0m to "$last_merge
 fi
 
 if [ ! -z "$closed" ]; then
 	echo ""
 	echo -e '⊺ \e[33mClosed\x1B[0m '$closed
 fi
-#7102c997c75a
